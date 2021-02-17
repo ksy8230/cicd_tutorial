34da0f5f4291e50bf60e5a5324004b8978665418
김수영 <ymt45@estsecurity.com>
2021-02-17 17:53:10 +0900

    pr 성공시키기

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
       ..
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          fdf Edit <code>src/Ap p.js</code> and save .to reload...
-        </p>
+        <p>this is feature 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn!!?
+          test
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders test link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).not.toBeInTheDocument();
+  const linkElement = screen.getByText(/test/i);
+  expect(linkElement).toBeInTheDocument();
 });
